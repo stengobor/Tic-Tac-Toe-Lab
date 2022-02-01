c333fc61d640041872edb3ff797182d4db8da628
Sophie Engobor <sophietengobor@gmail.com>
2022-02-01 14:12:20 -0600

    Made a lot of changes

--- app.js
+++ app.js
@@ -21,61 +21,114 @@ function cellClicked(event) {
             return;
         }
         currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT;
+        if (playersHaveDrawn()) {
+            playText.innerText = ('Draw!');
+            return;
+        }
     }
 }
 
-const playerHasWon = () => {
+
+const playersHaveDrawn = () => {
     if (spaces[0] === currentPlayer) {
-        if (spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
-            console.log(currentPlayer + " Wins on the Top");
+        if (spaces[1] === X_TEXT || O_TEXT && spaces[2] === O_TEXT || X_TEXT) {
+            console.log("Players Have Drawn");
             return (true);
         }
-        if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
-            console.log(currentPlayer + " Wins on the Left");
+        if (spaces[3] === X_TEXT || O_TEXT && spaces[6] === O_TEXT || X_TEXT) {
+            console.log("Players Have Drawn");
             return (true);
         }
-        if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
-            console.log(currentPlayer + " Wins Diagonaly");
-            return (true);
-        }
-    }
-    if (spaces[8] === currentPlayer) {
-        if (spaces[5] === currentPlayer && spaces[2] === currentPlayer) {
-            console.log(currentPlayer + " Wins on the Right");
-            return (true);
-        }
-        if (spaces[7] === currentPlayer && spaces[6] === currentPlayer) {
-            console.log(currentPlayer + " Wins on the Bottom");
+        if (spaces[4] === X_TEXT || O_TEXT && spaces[8] === O_TEXT || X_TEXT) {
+            console.log("Players Have Drawn");
             return (true);
         }
     }
     if (spaces[4] === currentPlayer) {
-        if (spaces[1] === currentPlayer && spaces[7] === currentPlayer) {
-            console.log(currentPlayer + " Wins Down Middle");
-            return (true);
+        if (spaces[1] === X_TEXT || O_TEXT && spaces[7] === O_TEXT || X_TEXT) {
+                console.log("Players Have Drawn");
+                return (true);
         }
-        if (spaces[3] === currentPlayer && spaces[5] === currentPlayer) {
-            console.log(currentPlayer + " Wins Across");
-            return (true);
+        if (spaces[3] === X_TEXT || O_TEXT && spaces[5] === O_TEXT || X_TEXT) {
+                console.log("Players Have Drawn");
+                return (true);
         }
-        if (spaces[2] === currentPlayer && spaces[6] === currentPlayer) {
-            console.log(currentPlayer + " Wins Across");
-            return (true);
+        if (spaces[2] === X_TEXT || O_TEXT && spaces[6] === O_TEXT || X_TEXT) {
+                console.log("Players Have Drawn");
+                return (true);
         }
-
     }
-};
-
-const reset = () => {
-    spaces.forEach((space, i) => {
-        spaces[i] = null;
-    })
-    cells.forEach(cell => {
-        cell.innerText = '';
-    })
-    playText.innerText = 'Tic Tac Toe';
-    currentPlayer = X_TEXT;
+    if (spaces[8] === currentPlayer) {
+        if (spaces[7] === X_TEXT || O_TEXT && spaces[6] === O_TEXT || X_TEXT) {
+                    console.log("Players Have Drawn");
+                    return (true);
+        }
+        if (spaces[5] === X_TEXT || O_TEXT && spaces[2] === O_TEXT || X_TEXT) {
+                    console.log("Players Have Drawn");
+                    return (true);
+        }
+        if (spaces[4] === X_TEXT || O_TEXT && spaces[0] === O_TEXT || X_TEXT) {
+                    console.log("Players Have Drawn");
+                    return (true);
+        }
+    }
 }
 
-resetButton.addEventListener ('click', reset);
-reset();
+
+
+
+        const playerHasWon = () => {
+            if (spaces[0] === currentPlayer) {
+                if (spaces[1] === currentPlayer && spaces[2] === currentPlayer) {
+                    console.log(currentPlayer + " Wins on the Top");
+                    return (true);
+                }
+                if (spaces[3] === currentPlayer && spaces[6] === currentPlayer) {
+                    console.log(currentPlayer + " Wins on the Left");
+                    return (true);
+                }
+                if (spaces[4] === currentPlayer && spaces[8] === currentPlayer) {
+                    console.log(currentPlayer + " Wins Diagonaly");
+                    return (true);
+                }
+            }
+            if (spaces[8] === currentPlayer) {
+                if (spaces[5] === currentPlayer && spaces[2] === currentPlayer) {
+                    console.log(currentPlayer + " Wins on the Right");
+                    return (true);
+                }
+                if (spaces[7] === currentPlayer && spaces[6] === currentPlayer) {
+                    console.log(currentPlayer + " Wins on the Bottom");
+                    return (true);
+                }
+            }
+            if (spaces[4] === currentPlayer) {
+                if (spaces[1] === currentPlayer && spaces[7] === currentPlayer) {
+                    console.log(currentPlayer + " Wins Down Middle");
+                    return (true);
+                }
+                if (spaces[3] === currentPlayer && spaces[5] === currentPlayer) {
+                    console.log(currentPlayer + " Wins Across");
+                    return (true);
+                }
+                if (spaces[2] === currentPlayer && spaces[6] === currentPlayer) {
+                    console.log(currentPlayer + " Wins Across");
+                    return (true);
+                }
+
+            }
+        }
+
+        const reset = () => {
+            spaces.forEach((space, i) => {
+                spaces[i] = null;
+            })
+            cells.forEach(cell => {
+                cell.innerText = '';
+            })
+            playText.innerText = 'Tic Tac Toe';
+            currentPlayer = X_TEXT;
+        }
+
+        resetButton.addEventListener('click', reset);
+        reset();
